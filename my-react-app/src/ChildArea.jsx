23873c7c8c64b69c8c33ce906d2a51f7c3ec87dc
komaitaira <komaitaira@gmail.com>
2021-12-05 20:33:50 +0900

    レンダリング最適化（メモ）

--- my-react-app/src/ChildArea.jsx
+++ my-react-app/src/ChildArea.jsx
@@ -1,10 +1,11 @@
+import { memo } from "react";
 const style = {
     width: "100%",
     height: "200px",
     backgroundColor: "khaki",
 };
 
-export const ChildArea = (props) => {
+export const ChildArea = memo((props) => {
     const { open } = props;
     const data = [...Array(100).keys()];
     console.log("ChildAreaがレンダリングされた");
@@ -20,4 +21,4 @@ export const ChildArea = (props) => {
             ) : null}
         </>
     );
-};
+});
