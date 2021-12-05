7d8d66f3ec0298696006364fd78adb4fb6266018
komaitaira <komaitaira@gmail.com>
2021-12-06 01:03:02 +0900

    stateを渡すページ遷移

--- section5/react-router-app/src/Page1.jsx
+++ section5/react-router-app/src/Page1.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
 export const Page1 = () => {
+    const array = [...Array(100).keys()];
     return (
         <div>
             <h1>Page1ページです。</h1>
-            <Link to="/page1/detailA">DetailA</Link>
+            <Link to={{ pathname: "/page1/detailA", state: array }}>
+                DetailA
+            </Link>
             <br />
             <Link to="/page1/detailB">DetailB</Link>
         </div>
--- section5/react-router-app/src/Page1DetailA.jsx
+++ section5/react-router-app/src/Page1DetailA.jsx
@@ -1,4 +1,6 @@
+import { useLocation } from "react-router";
 export const Page1DetailA = () => {
+    console.log(useLocation())
     return (
         <div>
             <h1>Page1DetailAページです。</h1>
