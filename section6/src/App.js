fe7243eaf61a99511251200464a81b8aa8045264
komaitaira <komaitaira@gmail.com>
2021-12-07 00:34:20 +0900

    Moleculeの作成

--- section6/src/App.js
+++ section6/src/App.js
@@ -1,10 +1,13 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
 export const App = () => {
     return (
         <div className="App">
             <PrimaryButton>テスト</PrimaryButton>
             <SecondaryButton>検索</SecondaryButton>
+            <br/>
+            <SearchInput />
         </div>
     );
 };
