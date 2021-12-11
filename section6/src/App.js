fdd24fa52d2d8a31b790d899eb98aa27be075532
komaitaira <komaitaira@gmail.com>
2021-12-11 23:19:23 +0900

    Pageの作成

--- section6/src/App.js
+++ section6/src/App.js
@@ -1,31 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayoyt";
+import { Router } from "./router/Router";
 import "./index.css";
 
-const user = {
-    name: "太郎",
-    image: "https://source.unsplash.com/G8cB8hY3yvU",
-    email: "test@example.com",
-    phone: "03-1111-2222",
-    company: {
-        name: "テスト株式会社",
-    },
-    website: "https://google.com",
-};
 export const App = () => {
-    return (
-        <BrowserRouter>
-            <DefaultLayout>
-                <PrimaryButton>テスト</PrimaryButton>
-                <SecondaryButton>検索</SecondaryButton>
-                <br />
-                <SearchInput />
-                <UserCard user={user} />
-            </DefaultLayout>
-        </BrowserRouter>
-    );
+    return <Router />;
 };
