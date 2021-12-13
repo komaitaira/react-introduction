d197f6251d5e914670f68ea04039bf0c7b84efaf
komaitaira <komaitaira@gmail.com>
2021-12-13 23:15:31 +0900

    React typescriptの準備

---
+++ section9/my-app/src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+    const onClickFetchData = () => {
+        axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+            console.log(res);
+        });
+    };
+    return (
+        <div className="App">
+            <button onClick={onClickFetchData}>データ取得</button>
+        </div>
+    );
+}
+
+export default App;
