71159545652b9553d76c7b44a4741cf3c170471d
komaitaira <komaitaira@gmail.com>
2021-12-19 20:15:18 +0900

    ユーザー取得機能の実装

--- section11/user-administration-app/src/components/pages/UserManagement.tsx
+++ section11/user-administration-app/src/components/pages/UserManagement.tsx
@@ -1,17 +1,29 @@
-import { Wrap, WrapItem } from "@chakra-ui/layout";
-import { memo, VFC } from "react";
+import { Wrap, WrapItem, Spinner, Center} from "@chakra-ui/react";
+import { memo, useEffect, VFC } from "react";
 import { UserCard } from "../organisms/user/UserCard";
-
+import { useAllUsers } from "../hooks/useAllUsers";
 export const UserManagement: VFC = memo(() => {
+    const { getUsers, users, loading } = useAllUsers();
+    useEffect(() => getUsers(), []);
     return (
-        <Wrap p={{ base: 4, md: 10 }}>
-            <WrapItem>
-                <UserCard
-                    imageUrl="https://source.unsplash.com/random"
-                    userName="田中　太郎"
-                    fullName="Taro Tanaka"
-                />
-            </WrapItem>
-        </Wrap>
+        <>
+            {loading ? (
+                <Center h="100vh">
+                    <Spinner />
+                </Center>
+            ) : (
+                <Wrap p={{ base: 4, md: 10 }} justify="space-around">
+                    {users.map((user) => (
+                        <WrapItem key={user.id}>
+                            <UserCard
+                                imageUrl="https://source.unsplash.com/random"
+                                userName={user.username}
+                                fullName={user.name}
+                            />
+                        </WrapItem>
+                    ))}
+                </Wrap>
+            )}
+        </>
     );
 });
