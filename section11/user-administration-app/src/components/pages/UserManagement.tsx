b0ddee9e5b4d6ef954a3d1b177f0ce0fcf2b7344
komaitaira <komaitaira@gmail.com>
2021-12-19 19:15:33 +0900

    ユーザー一覧画面の作成

--- section11/user-administration-app/src/components/pages/UserManagement.tsx
+++ section11/user-administration-app/src/components/pages/UserManagement.tsx
@@ -1,5 +1,17 @@
+import { Wrap, WrapItem } from "@chakra-ui/layout";
 import { memo, VFC } from "react";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
-    return <p>ユーザー管理ページです</p>;
+    return (
+        <Wrap p={{ base: 4, md: 10 }}>
+            <WrapItem>
+                <UserCard
+                    imageUrl="https://source.unsplash.com/random"
+                    userName="田中　太郎"
+                    fullName="Taro Tanaka"
+                />
+            </WrapItem>
+        </Wrap>
+    );
 });
